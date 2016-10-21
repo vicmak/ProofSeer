b3c35dfb84b070a9ef6a0bcc4218b1a5aefc35d4
Victor Makarenkov <vitiokm@gmail.com>
2016-10-21 11:24:24 +0300

    Add files via upload

--- ProofSeerLM.py
+++ ProofSeerLM.py
@@ -79,8 +79,11 @@ def get_tokenized_file_to_vectors(vocab, file2tokenize):
             if len(tokens) > 4:
                 five_gramms = find_ngrams(line, 5)
                 for gramm in five_gramms:
-                    str_gramm = get_csv(gramm, vocab)
-                    tokenized_file = tokenized_file + str_gramm
+                    current_y = gramm[2]
+                    current_y = clean_text(current_y)
+                    if current_y in vocab:
+                        str_gramm = get_csv(gramm, vocab)
+                        tokenized_file = tokenized_file + str_gramm
     return tokenized_file
 
 
@@ -106,7 +109,6 @@ def find_word_index(word, common_words_filename):
         print("Empty token")
     #return -1 if the word was not found in the list of most common words
     return -1
-
 
 def read_vocab_to_list(filename):
     return [word for line in open(filename, 'r') for word in line.split()]
@@ -157,7 +159,8 @@ def find_ngrams(s, n):
 
 def create_online_dataset(filename):
 
-    np.genfromtxt(StringIO(data), delimiter=",")
+    np.genfromtxt(StringIO(filename), delimiter=",")
+
 
 def getModel(one_hot_train_filename):
 
@@ -214,19 +217,52 @@ def train_model_from_dir(root, vocabulary_filename):
         for name in files:
             current_filename = os.path.join(path, name)
             if current_filename.endswith("txt"):
+                if find_word_index(current_filename, "/Users/macbook/Desktop/corpora/aux_files/log.txt") == -1:
+                    count = count + 1
+                    print("file number", count)
+                    data = get_tokenized_file_to_vectors(vocabulary_filename, current_filename)
+                    dataset = np.genfromtxt(StringIO(data), delimiter=",")
+                    X = dataset[:, 0:10000]
+                    Y = dataset[:, 10000:]
+                    arrX = np.array(X)
+                    arrY = np.array(Y)
+                    model.fit(arrX, arrY, nb_epoch=30, batch_size=dataset.shape[0]) #check the batch size
+                    log_train_file(current_filename, dataset.shape[0])
+                    if count % 10 == 0:
+                        print("Saving model...", count)
+                        model.save("/Users/macbook/Desktop/corpora/aux_files/model.h5")
+                else:
+                    print("file already trained:", current_filename)
+    end_time = time.time()
+    print("elapsed time", end_time - start_time)
+    model.save("/Users/macbook/Desktop/corpora/aux_files/model-final.h5")
+
+
+def continue_train_model_from_dir(root, vocabulary_filename, model_filename):
+
+    start_time = time.time()
+    model = load_model(model_filename)
+    count = 0
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            current_filename = os.path.join(path, name)
+            if current_filename.endswith("txt"):
                 count = count + 1
-                print("file number", count)
-                data = get_tokenized_file_to_vectors(vocabulary_filename, current_filename)
-                dataset = np.genfromtxt(StringIO(data), delimiter=",")
-                X = dataset[:, 0:10000]
-                Y = dataset[:, 10000:]
-                arrX = np.array(X)
-                arrY = np.array(Y)
-                model.fit(arrX, arrY, nb_epoch=30, batch_size=dataset.shape[0]) #check the batch size
-                log_train_file(current_filename, dataset.shape[0])
-                if count % 10 == 0:
-                    print("Saving model...", count)
-                    model.save("/Users/macbook/Desktop/corpora/aux_files/model.h5")
+                if find_word_index(current_filename, "/Users/macbook/Desktop/corpora/aux_files/log.txt") == -1:
+                    print("file number", count)
+                    data = get_tokenized_file_to_vectors(vocabulary_filename, current_filename)
+                    dataset = np.genfromtxt(StringIO(data), delimiter=",")
+                    X = dataset[:, 0:10000]
+                    Y = dataset[:, 10000:]
+                    arrX = np.array(X)
+                    arrY = np.array(Y)
+                    model.fit(arrX, arrY, nb_epoch=30, batch_size=dataset.shape[0]) #check the batch size
+                    log_train_file(current_filename, dataset.shape[0])
+                    if count % 10 == 0:
+                        print("Saving model...", count)
+                        model.save("/Users/macbook/Desktop/corpora/aux_files/model.h5")
+                else:
+                    print("file already trained:", count, current_filename)
     end_time = time.time()
     print("elapsed time", end_time - start_time)
     model.save("/Users/macbook/Desktop/corpora/aux_files/model-final.h5")
@@ -240,57 +276,155 @@ def log_train_file(filename, tokens_num):
 
 
 def test_model_on_dir(model_filename, root, vocab):
-    count = 0
     model = load_model(model_filename)
-    print("model loaded")
     for path, subdirs, files in os.walk(root):
         for name in files:
-
-            print("file name", os.path.join(path, name))
-            data = get_tokenized_file_to_vectors(vocab, os.path.join(path, name))
-            dataset = np.genfromtxt(StringIO(data), delimiter=",")
-            print("loaded dataset")
-            X = dataset[:, 0:10000]
-            Y = dataset[:, 10000:]
-            arrX = np.array(X)
-            arrY = np.array(Y)
-            predictions = model.predict(arrX)
-            mrr = getMRR(predictions, arrY)
-            print("MRR:", mrr)
+          #  print("file name", os.path.join(path, name))
+            if (name.endswith("txt")):
+                data = get_tokenized_file_to_vectors(vocab, os.path.join(path, name))
+                dataset = np.genfromtxt(StringIO(data), delimiter=",")
+                X = dataset[:, 0:10000]
+                Y = dataset[:, 10000:]
+                arrX = np.array(X)
+                arrY = np.array(Y)
+                predictions = model.predict(arrX)
+                mrr = getMRR_after_sort(predictions, arrY, arrX, vocab)
+              #  mrr = getMRR(predictions, arrY)
+                print(name, "MRR:", mrr)
 
 
 def getMRR(predictions, labels):
-    count = 0
     sum = 0
-    for i in range(0,len(predictions), 1):
+    for i in range(0, len(predictions), 1):
         correct_index = np.argmax(labels[i])
         predicted_index = np.argmax(predictions[i])
-     #   print("correct and predicted:", correct_index, predicted_index)
         rank = 1
-        count = 0
-        while (predicted_index != correct_index and rank<len(predictions)):
-            predictions[i][predicted_index]= -1
+        while predicted_index != correct_index and rank < len(predictions):
+            predictions[i][predicted_index] = -1
             predicted_index = np.argmax(predictions[i])
-         #   print("predicted index:", predicted_index)
             rank = rank + 1
         if rank < len(predictions)-1:
             sum = sum + 1/float(rank)
     return sum/float(len(predictions))
 
 
+def getMRR_after_sort(predictions, labels, contexts, vocab):
+    sum = 0
+    for i in range(0, len(predictions), 1):
+        sorted_predictions = get_top_sorted_predictions_indexes(predictions[i], contexts[i], vocab) #sorted 1-dimensional array of suggestions
+        correct_index = np.argmax(labels[i])
+        rank = 1
+        predicted_index = sorted_predictions[0]
+        while predicted_index != correct_index and rank < len(sorted_predictions):
+            predicted_index = sorted_predictions[rank]
+            rank = rank + 1
+        if rank < len(predictions)-1:
+            sum = sum + 1/float(rank)
+    return sum/float(len(predictions))
+
+
+def get_top_sorted_predictions_indexes(prediction, context, vocab):
+    sorted_predictions = [0] * 5
+    top_ten_indices = [0] * 5
+    for i in range(0, 5, 1):
+        max_probability_index = np.argmax(prediction)
+        top_ten_indices[i] = max_probability_index
+        prediction[max_probability_index] = -1
+
+    top_ten_words = []
+    for index in top_ten_indices:
+        top_ten_words.append(vocab[index])
+    top_ten_vectors = []
+    for word in top_ten_words:
+        vector = get_vector(word)
+        top_ten_vectors.append(vector)
+
+    context_indexes = np.where(context == 1)
+ #   print ("length", context_indexes[0])
+    context_words = []
+    for index in context_indexes[0]:
+        context_words.append(vocab[index])
+    context_vectors = []
+    for word in context_words:
+        vector = get_vector(word)
+        context_vectors.append(vector)
+
+    context_similarities = []
+    for vector in top_ten_vectors:
+        vector_sim = 0
+        context_words_num = 0
+        for context_vec in context_vectors:
+            vector_sim = vector_sim + 1 - spatial.distance.cosine([float(i) for i in vector], [float(i) for i in context_vec])
+            context_words_num = context_words_num + 1
+        grade = 0
+        if context_words_num > 0:#in case no context words
+        #    print("Context words num:", context_words_num)
+            grade = vector_sim / float(context_words_num)
+        context_similarities.append(grade)
+    decorated = zip(top_ten_indices, context_similarities)
+    list_of_lists = [list(elem) for elem in decorated]
+    sorted_predictions = sorted(list_of_lists, key=lambda pair: pair[1], reverse=True)
+    list1, list2 = zip(*sorted_predictions)
+    return list1
+
+
+def get_vector(token, extra_vectors_file="/Users/macbook/Desktop/corpora/aux_files/extra_vocab.txt"):
+    config = RNNGloveConfig()
+    with open(extra_vectors_file) as f:
+        for line in f:
+            tokens = line.split()
+            if tokens[0] == token.lower():
+                vec = tokens[1:config.vector_dimension+1]
+    #            print ("returning from extra: ", token)
+                return vec
+    with open(config.glove_vectors) as f:
+        for line in f:
+            tokens = line.split()
+            if tokens[0] == token.lower():
+                vec = tokens[1:config.vector_dimension+1]
+                return vec
+    vec = add_unseen_token_2_extra_vocabulary(token, extra_vectors_file)
+    return vec
+
+
+def add_unseen_token_2_extra_vocabulary(token, extra_vocab_filename):
+    config = RNNGloveConfig()
+ #   print("Adding unseen token: ", token)
+    random_vector = [random.random() for _ in range(0, config.vector_dimension)]
+    string_vector = [str(i) for i in random_vector]
+    vector = [token.lower()] + [" "] + string_vector
+    with open(extra_vocab_filename, "a") as myfile:
+        str_vector = ' '.join(str(e) for e in vector) #covert list to string
+        str_vector = str_vector + "\n"
+        myfile.write(str_vector)
+    return random_vector
+
+
+def get_context_words(context):
+    words = [""] * 5
+    return words
+
+class RNNGloveConfig(object):
+    vector_dimension = 50
+    epochs_number = 50
+    glove_vectors = "/Users/macbook/Desktop/corpora/aux_files/glove.6B.50d.txt"
+
 def main():
 
     print("haha")
 
     common_words_filename = "/Users/macbook/Desktop/corpora/aux_files/vocab10000.txt"
+    dense_vectors_glove = "/Users/macbook/Desktop/corpora/aux_files/glove.6B.50d.txt"
     text_from_pdf_file = "/Users/macbook/Desktop/corpora/aux_files/text_from_pdf_dir.txt"
     file_2_tokenize_name = "/Users/macbook/Desktop/corpora/aux_files/qpp1.txt"
     one_hot_train_data = "/Users/macbook/Desktop/corpora/aux_files/one_hot_csv.txt"
 
-
     vocab = read_vocab_to_list(common_words_filename)
-    train_model_from_dir("/Users/macbook/Desktop/corpora/corpus30k", vocab)
- #   test_model_on_dir("/Users/macbook/Desktop/corpora/aux_files/model.h5", "/Users/macbook/Desktop/corpora/tiny_test", vocab)
+   # continue_train_model_from_dir("/Users/macbook/Desktop/corpora/corpus30k",
+   #                               vocab,
+   #                               "/Users/macbook/Desktop/corpora/aux_files/model.h5")
+    #train_model_from_dir("/Users/macbook/Desktop/corpora/corpus30k", vocab)
+    test_model_on_dir("/Users/macbook/Desktop/corpora/aux_files/model.h5", "/Users/macbook/Desktop/corpora/triple_test", vocab)
 
 
   #  print(data)
