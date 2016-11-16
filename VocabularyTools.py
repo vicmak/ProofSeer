a0f6b76253a2bca3fb48a77d264ccb962d7624f2
Victor Makarenkov <vitiokm@gmail.com>
2016-11-16 07:56:08 +0200

    Add files via upload

--- VocabularyTools.py
+++ VocabularyTools.py
@@ -26,7 +26,7 @@ def clean_text(token):
     return token
 
 
-def get_clean_text_from_file(filename, stop_words_list):
+def get_clean_text_from_file(filename, stop_words_list=None):
 
     file_content = ""
     with open(filename) as f:
@@ -36,13 +36,59 @@ def get_clean_text_from_file(filename, stop_words_list):
             for token in tokens:
                 clean_token = clean_text(token)
                 if len(clean_token) > 1 and clean_token.isalpha():
-                    if clean_token not in stop_words_list:
+                    if stop_words_list != None:
+                        if clean_token not in stop_words_list:
+                            file_content = file_content + clean_token + " "
+                    else:
                         file_content = file_content + clean_token + " "
+    return file_content
+
+
+def get_clean_text_from_file2(filename):
+
+    file_content = ""
+    count = 0
+    with open(filename) as f:
+        for line in f:
+            count = count + 1
+            if count > 245:
+                file_content = file_content + line.lower() + " "
     return file_content
 
 
 def read_vocab_to_list(filename):
     return [word for line in open(filename, 'r') for word in line.split()]
+
+
+def clean_mscc(source_dir, target_dir):
+    count = 0
+    for root, dirs, files in os.walk(source_dir):
+        path = root.split('/')
+        print (len(path) - 1) *'---' , os.path.basename(root)
+        for file in files:
+            if file.endswith("TXT"):
+                count = count + 1
+                print("count: ", count)
+                current_source_file_name = source_dir + "/" + file
+                target_file_text = get_clean_text_from_file2(current_source_file_name)
+                current_target_file_name = target_dir + "/" + file
+                with io.FileIO(current_target_file_name, "w") as file_target:
+                    file_target.write(target_file_text)
+
+
+def create_single_file(source_dir, target_filename):
+    count = 0
+    for root, dirs, files in os.walk(source_dir):
+        path = root.split('/')
+        print (len(path) - 1) *'---' , os.path.basename(root)
+        for file in files:
+            if file.endswith("TXT"):
+                count = count + 1
+                print("count: ", count)
+                current_source_file_name = source_dir + "/" + file
+                target_file_text = get_clean_text_from_file(current_source_file_name)
+                with io.FileIO(target_filename, "a") as file_target:
+                    file_target.write(target_file_text)
 
 
 def clean_corpus(source_dir, target_dir, stop_words_list):
@@ -51,7 +97,7 @@ def clean_corpus(source_dir, target_dir, stop_words_list):
         path = root.split('/')
         print (len(path) - 1) *'---' , os.path.basename(root)
         for file in files:
-            if file.endswith("txt"):
+            if file.endswith("TXT"):
                 count = count + 1
                 print("count: ", count)
                 current_source_file_name = source_dir + "/" + file
@@ -68,7 +114,7 @@ def extract_vocabulary(dir_name, targetTextFileName="bla"):
         path = root.split('/')
         print (len(path) - 1) *'---' , os.path.basename(root)
         for file in files:
-            if (file.endswith("txt")):
+            if (file.endswith("TXT")):
                 count = count + 1
                 print("count: ", count)
                 with open(dir_name + "/" + file) as f:
@@ -93,15 +139,17 @@ def write_vocab_2_file(filename, vocab):
 
 def main():
 
-    stop_words = read_vocab_to_list("/Users/macbook/Desktop/corpora/aux_files/stop_words.txt")
-    print(stop_words)
-    clean_corpus("/Users/macbook/Desktop/corpora/triple_test", "/Users/macbook/Desktop/corpora/target_dir", stop_words)
+    #stop_words = read_vocab_to_list("/Users/macbook/Desktop/corpora/aux_files/mscc_stop_words.txt")
+    #print(stop_words)
+    #clean_corpus("/Users/macbook/Desktop/corpora/mscc_text", "/Users/macbook/Desktop/corpora/mscc_clean", stop_words)
+    #clean_mscc("/Users/macbook/Desktop/corpora/mscc_train", "/Users/macbook/Desktop/corpora/mscc_text")
+    create_single_file("/Users/macbook/Desktop/corpora/mscc_clean", "/Users/macbook/Desktop/corpora/mscc_single_clean.txt")
     #print("vocab tools")
-    #vocab = extract_vocabulary("/Users/macbook/Desktop/corpora/corpus30k")
+  #  vocab = extract_vocabulary("/Users/macbook/Desktop/corpora/mscc_clean")
     #print(vocab)
     #print(len(vocab))
-    #common_words_filename = "/Users/macbook/Desktop/corpora/aux_files/vocab.txt"
-    #write_vocab_2_file(common_words_filename, vocab)
+   # common_words_filename = "/Users/macbook/Desktop/corpora/aux_files/mscc_vocab.txt"
+   # write_vocab_2_file(common_words_filename, vocab)
 
 
 if __name__ == "__main__":
